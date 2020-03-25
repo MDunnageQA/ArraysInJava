2d3ae4d5cbd16a1903e5c48f12aee8ee532185ed
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-03-25 15:53:54 +0000

    First Part

---
+++ src/com/qa/main/Arrays.java
@@ -0,0 +1,13 @@
+package com.qa.main;
+
+public class Arrays
+{
+	public void outputArray(int numberArray[])
+	{
+		System.out.println(numberArray[0] + " " + numberArray[1] + " " + numberArray[2] +
+				" " + numberArray[3] + " " + numberArray[4] + " " + numberArray[5] + " " +
+				numberArray[6] + " " + numberArray[7] + " " + numberArray[8] + " " +
+				numberArray[9]);
+	}
+
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,15 @@
+package com.qa.main;
+
+public class Runner 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int [] numberArray = {3, 5, 6, 1, 2, 9, 7, 8, 4, 3};
+		Arrays user = new Arrays();
+		user.outputArray(numberArray);
+
+	}
+
+}
